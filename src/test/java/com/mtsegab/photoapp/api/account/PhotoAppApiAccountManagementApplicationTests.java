a20b2a8b95d794936c8694788c88f099680b7826
tsegabmekonnen <tsegabmekonnen@gmail.com>
2023-12-11 07:38:03 -0600

    commit-1 : adds endpoints for account management service. WIP.

---
+++ src/test/java/com/mtsegab/photoapp/api/account/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtsegab.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
